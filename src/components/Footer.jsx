01956ff2e2bc252a54e5a5e57b3680814bdfa3b8
alanjoy777 <alenj235@gmail.com>
2024-10-01 17:27:38 +0530

    e-kart

---
+++ src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Footer = () => {
+  return (
+    <footer className="bg-body-tertiary fixed-bottom text-dark mt-5 p-4">
+      <Container style={{fontWeight:"bold"}}>
+        <Row>
+          <Col md="4">
+            <h5>E-Cart</h5>
+            <p>Your one-stop shop for all your needs!</p>
+          </Col>
+          <Col md="4">
+            <h5>Quick Links</h5>
+            <ul className="list-unstyled">
+              <li><a href="/" className="text-dark">Home</a></li>
+              <li><a href="/shop" className="text-dark">Shop</a></li>
+              <li><a href="/contact" className="text-dark">Contact Us</a></li>
+              <li><a href="/about" className="text-dark">About Us</a></li>
+            </ul>
+          </Col>
+          <Col md="4">
+            <h5>Contact Us</h5>
+            <p>Email: support@e-cart.com</p>
+            <p>Phone: +1 234 567 890</p>
+            <p>Address: 123 E-Cart Street, Shop City</p>
+          </Col>
+        </Row>
+        <Row className="mt-3">
+          <Col className="text-center">
+            <p className="mb-0">&copy; {new Date().getFullYear()} E-Cart. All Rights Reserved.</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
+import Badge from 'react-bootstrap/Badge';
+function Header() {
+  return (
+    <div>
+        <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home" >
+          <Link to={'/'} style={{textDecoration:"none",color:"black",fontWeight:"bold"}}>
+             <i className='fa-solid fa-truck-fast fa-bounce'></i> E-CART
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link  className='btn btn-outline-light'>
+              <Link to={'/wishlist'} style={{color:"black",fontWeight:"bold",textDecoration:"none"} }>
+               <i className='fa-solid fa-heart text-danger'></i>Wishlist <Badge bg="success rounded ms-2">9</Badge>
+              </Link>
+            </Nav.Link>
+            <Nav.Link  className='btn btn-outline-light'>
+              <Link to={'/cart'} style={{color:"black",fontWeight:"bold",textDecoration:"none"} }>
+               <i className='fa-solid fa-cart-shopping text-warning'></i>Cart <Badge bg="success rounded ms-2">9</Badge>
+              </Link>
+            </Nav.Link>
+          
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
+  )
+}
+
+export default Header
